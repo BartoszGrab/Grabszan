70f59d4ebba5c5cd3a2a5b733b52ef9d4f9a92de
michalChinczyk <michalszandarowski@gmail.com>
2024-12-05 13:17:24 +0100

    command handling, singleton serwer, plansza

---
+++ trylma/src/main/java/grab/szan/gameModes/GameMode.java
@@ -0,0 +1,7 @@
+package grab.szan.gameModes;
+
+public interface GameMode {
+    //TODO kazdy tryb gry powinien miec metode do rozkladania pionkow
+    //TODO kazdy tryb gry powinien miec inny sposob sprawdzania czy gra ma zwyciezce
+    //TODO oprocz tego stworz klasy OneVsOne itp. ktore beda implementowaly ten interfejs
+}
