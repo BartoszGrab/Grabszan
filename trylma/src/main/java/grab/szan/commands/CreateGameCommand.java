c1570af95453c63365457e65ff5bcd117ea9c6fb
michalChinczyk <michalszandarowski@gmail.com>
2024-12-05 13:41:32 +0100

    Player dodany jako argument do metody execute w Command

--- trylma/src/main/java/grab/szan/commands/CreateGameCommand.java
+++ trylma/src/main/java/grab/szan/commands/CreateGameCommand.java
@@ -1,12 +1,14 @@
 package grab.szan.commands;
 
+import grab.szan.Player;
+
 /*
  * komenda do tworzenia nowej rozgrywki
  */
 public class CreateGameCommand implements Command{
 
     @Override
-    public void execute(String[] args) { 
+    public void execute(String[] args, Player player) { 
         //args[0] zawsze bedzie typem komendy czyli w tym przypadku
         //args[0] = "create", dopiero kolejne argumenty to parametry
         // TODO funkcja powinna przyjmowac jako argumenty nazwe rozgrywki, oraz liczbe graczy
