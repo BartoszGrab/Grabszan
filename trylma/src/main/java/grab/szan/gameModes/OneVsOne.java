d4cde165a6af2d4a4bf5cf2eee554fb03bf799d0
michalChinczyk <michalszandarowski@gmail.com>
2024-12-08 11:17:06 +0100

    OneVsOne interface implemented

--- trylma/src/main/java/grab/szan/gameModes/OneVsOne.java
+++ trylma/src/main/java/grab/szan/gameModes/OneVsOne.java
@@ -1,21 +1,61 @@
 package grab.szan.gameModes;
 
 import java.util.List;
-
 import grab.szan.Player;
 import grab.szan.boards.Board;
+import grab.szan.boards.ClassicBoard;
 
 public class OneVsOne implements GameMode {
 
+    private Player player1;
+    private Player player2;
+    private ClassicBoard classicBoard;
+
     @Override
     public void initializePieces(Board board, List<Player> players) {
-        // TODO: Zaimplementować rozstawianie 9 pionków dla każdego z graczy w odpowiednich narożach planszy.
+        classicBoard = (ClassicBoard) board;
+
+        player1 = players.get(0);
+        player2 = players.get(1);
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 12-i; j <= 12+i; j += 2){
+                classicBoard.getField(i, j).setPlayer(player1);
+            }
+        }
+
+        for(int i = 13; i <= 17; i++){
+            for(int j = i-4; j <= 28-i; j += 2){
+                classicBoard.getField(i, j).setPlayer(player2);
+            }
+        }
     }
 
     @Override
     public Player getWinner(Board board, List<Player> players) {
-        // TODO: Zaimplementować logikę sprawdzającą czy wszystkie pionki jednego z graczy
-        // znalazły się w strefie startowej przeciwnika.
+        int count = 0;
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 12-i; j <= 12+i; j += 2){
+                if(classicBoard.getField(i, j).getPlayer().equals(player2)){
+                    count++;
+                }
+            }
+        }
+
+        if(count == 10) return player2;
+        count = 0;
+
+        for(int i = 13; i <= 17; i++){
+            for(int j = i-4; j <= 28-i; j += 2){
+                if(classicBoard.getField(i, j).getPlayer().equals(player1)){
+                    count++;
+                }
+            }
+        }
+
+        if(count == 10) return player1;
+
         return null;
     }
 }
